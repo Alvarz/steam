f298c6ac3ad34c029857636a45945ca05a86f388
Carlos Alvarez <carlosxviii@gmail.com>
2018-09-25 18:19:49 -0300

    working with udp

--- src/UdpClient.ts
+++ src/UdpClient.ts
@@ -2,15 +2,15 @@ import * as dgram  from 'dgram'
 
 export default class UdpClient{
 
-  private client;
-  private PORT;
-  private HOST;
+  private static client;
+  private static PORT;
+  private static HOST;
 
-  constructor(){
+  /* constructor(){
 
     this.PORT = 33333;
     this.HOST = '127.0.0.1';
-  }
+  }*/
   /*
    *
    *
@@ -19,19 +19,25 @@ export default class UdpClient{
    *
    *
    * */
-  sendMessage(message){
+  public static sendMessage(message){
 
-    const self = this;
-    this.client = dgram.createSocket('udp4');
+    const self = UdpClient;
+    self.PORT = 33333;
+    self.HOST = '127.0.0.1';
+    self.client = dgram.createSocket('udp4');
     let msg = new Buffer(message);
-    this.client.send(msg, 0, msg.length,  self.PORT, self.HOST, function(err, bytes){
+    self.client.send(msg, 0, msg.length,  self.PORT, self.HOST, function(err, bytes){
 
       if(err)
         throw err;
       console.log('UDP message sent to ' + self.HOST +':'+ self.PORT);
-      self.client.close();
-    
+      //self.client.close();
+      setTimeout(function(){ self.client.close(); }, 1000);
     })
+
+    self.client.on('close', function() {
+        console.log('Client UDP socket closed : BYE!')
+    });
   
   }
 
